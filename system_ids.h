b95ecdd9adcfc4238fda8ebb56eb9c6bdbe0c2bf
Trent Lukaczyk <aerialhedgehog@gmail.com>
2014-11-26 02:47:27 -0800

    adding jaycee as author

--- system_ids.h
+++ system_ids.h
@@ -8,8 +8,12 @@
  * Defines the system id and component id
  *
  * @author Trent Lukaczyk, <aerialhedgehog@gmail.com>
+ * @author Jaycee Lock, <jaycee.lock@gmail.com>
  *
  */
+
+#ifndef SYSTEM_IDS_H_
+#define SYSTEM_IDS_H_
 
 
 // ------------------------------------------------------------------------------
@@ -21,4 +25,4 @@ int autopilot_compid = 50;  // The autopilot component (parameter: MAV_COMP_ID)
 int compid           = 355; // The offboard computer component ID
 
 
-
+#endif // SYSTEM_IDS_H_
